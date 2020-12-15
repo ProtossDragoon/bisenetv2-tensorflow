cc60a6163914c9bdd1196f725e0671ffcc0bf2d0
deer <janghoo.lee@deering.co>
2020-12-15 15:42:32 +0900

    Important bugfix

--- tools_ros/cityscapes/io.py
+++ tools_ros/cityscapes/io.py
@@ -32,7 +32,6 @@ import numpy as np
 
 # import ROS package
 from cv_bridge import CvBridge
-bridge = CvBridge()
 from sensor_msgs.msg import Image
 import rospy
 
@@ -53,29 +52,34 @@ def callback(data):
     global output_names
     global output_nodes
     global pub
+    global bridge
+    global runtime_session
+    global tracking_output_node_list 
     cv2_img = bridge.imgmsg_to_cv2(data, desired_encoding = 'passthrough')
-    preprocessed_image = preprocess_image(cv2_img)
-    result = sess.run(output_nodes, feed_dict={'tftrt/input_tensor:0':[preprocessed_image]})
+    preprocessed_image = preprocess_image(cv2_img, [1024, 512]) # FIXME : hardcoded size
+    result = runtime_session.run(output_nodes, feed_dict={'tftrt/input_tensor:0':[preprocessed_image]})
     for i in range(len(result)):
-        print('result {} - tensor name <{}> shape : {}'.format(i, outputs[i], result[i].shape))
+        print('result {} - tensor name <{}> shape : {}'.format(i, tracking_output_node_list[i], result[i].shape))
     
-    # ---------- 고쳐야함
     rate = rospy.Rate(10) # 10hz
-    bridge = CvBridge()
+    
 
     print(type(result))
     ch = 0
     ku_img_msg = bridge.cv2_to_imgmsg(result[1,:,:,ch:ch+3], encoding = 'passthrough')
     pub.publish(ku_img_msg)
-    # ------------
 
 
-def run_segmentation(trt_graph_def, tracking_output_node_list=['final_output:0']):
+def run_segmentation(trt_graph_def):
     
     # Import the TensorRT graph into a new graph and run:
     global output_names
     global output_nodes
     global pub
+    global bridge
+    global runtime_session
+    global tracking_output_node_list
+    bridge = CvBridge()
 
     tf.reset_default_graph()
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
@@ -91,19 +95,25 @@ def run_segmentation(trt_graph_def, tracking_output_node_list=['final_output:0']
         # NOTE: the name must be a base name, i.e. it cannot contain any slashes "/".
         # anonymous = True ensures that your node has a unique name by adding random numbers to the end of NAME.
     pub = rospy.Publisher('segmentation_chatter_pub', Image, queue_size=10)
-    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-        rospy.Subscriber('/camera/color/image_raw', Image, callback)
-        rospy.spin()
+    runtime_session = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    rospy.Subscriber('/camera/color/image_raw', Image, callback)
+    rospy.spin()
+    runtime_session.close()
 
 
 def main(args):
+    global runtime_session
+    global tracking_output_node_list 
+
     try:
         trt_graph_def = get_trt_graph_def(args.frozen_graph_path)
         print_default_graph_nodes_name()
         tracking_output_node_list = ['final_output:0', 'BiseNetV2/prob:0']        
-        run_segmentation(trt_graph_def, tracking_output_node_list=tracking_output_node_list)
+        run_segmentation(trt_graph_def)
 
     except rospy.ROSInterruptException:
+        if not sess._closed:
+            runtime_session.close()
         # this catches a rospy.ROSInterruptException exception
         # 해석하면.. 정상적 종료에 대해서 오류메시지 출력하고 그러지 않겠다는 뜻.
         pass
