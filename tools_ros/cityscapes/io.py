53ea9694bb923d0b082c062ed3b2370722f37104
deer <janghoo.lee@deering.co>
2020-12-16 13:52:29 +0900

    ROS Really Really Works ! + Add time tracker

--- tools_ros/cityscapes/io.py
+++ tools_ros/cityscapes/io.py
@@ -29,6 +29,7 @@ assert tf.__version__.startswith('1')
 print(tf.__version__)
 import cv2
 import numpy as np
+import time
 
 # import ROS package
 from cv_bridge import CvBridge
@@ -48,24 +49,76 @@ def init_args():
     return parser.parse_args()
 
 
+def time_logger(time_dict):
+    total_time = sum(list(time_dict.values()))
+    for idx, key in enumerate(list(time_dict.keys())):
+        print('[time] {}.{} : ({}%) {}s'.format(idx, key, round(time_dict[key]*100 / total_time, 3), round(time_dict[key], 3)))
+    print('[time] {}.total : {}s ({} FPS)'.format(idx + 1, round(total_time, 3), round(1/total_time, 2)))
+
+
+def datatype_converter_for_publisher(modeloutput, dtype_result, dtype_target):
+    print('result i : {}({}) -> '.format(modeloutput.shape, modeloutput.dtype), end='')
+    if str(dtype_result) == 'float32' and str(dtype_target) == 'uint8':
+        modeloutput = modeloutput.astype(np.float64) / modeloutput.max()
+        modeloutput = (255 * modeloutput).astype(np.uint8)
+        print('publish : {}({})'.format(modeloutput.shape, modeloutput.dtype))
+        return modeloutput
+    elif str(dtype_result) == 'float16':
+        raise NotImplementedError
+    elif str(dtype_result) == 'uint8':
+        raise NotImplementedError        
+    else :
+        raise NotImplementedError
+
+
 def callback(data):
     global output_names
     global output_nodes
     global pub
     global bridge
     global runtime_session
-    global tracking_output_node_list 
+    global tracking_output_node_list
+
     cv2_img = bridge.imgmsg_to_cv2(data, desired_encoding = 'passthrough')
     print('recieved image topic : {}'.format(cv2_img.dtype))
+
+    # TODO : time checker refactoring with decorator
+    time_dict = {}
+
+    t_start_preprocess = time.time()
     preprocessed_image = preprocess_image(cv2_img, [1024, 512]) # FIXME : hardcoded size
+    t_end_preprocess = time.time()
+    time_dict['preprocessing'] = t_end_preprocess - t_start_preprocess
+
+    t_start_inference = time.time()
     result = runtime_session.run(output_nodes, feed_dict={'tftrt/input_tensor:0':[preprocessed_image]})
+    t_end_inference = time.time()
+    time_dict['inference'] = t_end_inference - t_start_inference
+
+    # result   : list
+    # result[i]: np.ndarray
     for i in range(len(result)):
         print('result {} - tensor name <{}> shape : {}({})'.format(i, tracking_output_node_list[i], result[i].shape, result[i].dtype))
-     
+    
     ch = 0
-    ku_img_msg = bridge.cv2_to_imgmsg(result[1][0,:,:,ch:ch+3], encoding = 'bgr8')
-    print('pubslished image topic : {}'.format(result[1][0,:,:,ch:ch+3].shape))
+    interested_classes_segmentation_result = result[1][0,:,:,ch:ch+3]
+
+    t_start_dtypeconvert = time.time()
+    res = datatype_converter_for_publisher(interested_classes_segmentation_result, result[1].dtype, 'uint8')    
+    t_end_dtypeconvert = time.time()
+    time_dict['dtypeconvert'] = t_end_dtypeconvert - t_start_dtypeconvert
+
+    t_start_bridge = time.time()
+    ku_img_msg = bridge.cv2_to_imgmsg(res, encoding = 'passthrough')
+    t_end_bridge = time.time()
+    time_dict['bridge'] = t_end_bridge - t_start_bridge
+
+    print('pubslished image topic : {}({})'.format(res.shape, res.dtype))
+    
     pub.publish(ku_img_msg)
+
+    time_logger(time_dict)
+
 
 
 def run_segmentation(trt_graph_def):
